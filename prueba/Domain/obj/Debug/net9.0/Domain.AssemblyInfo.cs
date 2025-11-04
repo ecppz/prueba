1f3170e8a4d66a67072569dc388b05fffc62079e
EP <pinedajaja0@gmail.com>
2025-11-04 17:20:50 -0400

    nueva interfaz en service

--- prueba/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ prueba/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7113cb06179ccbe69e00e74b4edf91379a34ea8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1d4701bce7d42ee265197b0791bd40b3b55efad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
